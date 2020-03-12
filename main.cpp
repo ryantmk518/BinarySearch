920340cd605dbf7750441b015b98b24ef30603f1
Ryan Thammakhoune <ryantmk518@gmail.com>
2020-03-12 17:12:46 +0100

    started print

--- main.cpp
+++ main.cpp
@@ -7,68 +7,96 @@
 using namespace std;
 
 void add(Node* parent, int value, int side);
-void print(Node* head);
+void print(Node* head, int space);
 
 int main(){
+  bool quit = false;
   Node* head = new Node(NULL);
-  cout << "Console or File?" << endl;
-  char input[99];
-  cin.getline(input, 99);
-  if (strcmp(input, "Console") == 0) {
-    cout << "Enter input separated by spaces" << endl;
-    char in[999];
-    cin.getline(in, 999);
-    int* array = new int[999];
-    int a = 0;
-    char* token = strtok(in, " ");
-    while (token != NULL) {
-      array[a] = atoi(token);
-      token = strtok(NULL, " ");
-      ++a;
+  while (quit == false) {
+    cout << "Enter Console, File, or Quit" << endl;
+    char input[99];
+    cin.getline(input, 99);
+    if (strcmp(input, "Console") == 0) {
+      cout << "Enter input separated by spaces" << endl;
+      char in[999];
+      cin.getline(in, 999);
+      int* array = new int[999];
+      int a = 0;
+      char* token = strtok(in, " ");
+      while (token != NULL) {
+        array[a] = atoi(token);
+        token = strtok(NULL, " ");
+        ++a;
+      }
+      if (head == NULL) {
+        add(head, array[0], 2);
+        for(int i = 1; i < a; i++) {
+          add(head, array[i], 0);
+        }
+      }
+      else {
+        for (int i =0; i<a; i++) {
+          add(head, array[0], 0);
+        }
+      }
+      print(head, 0);
+      cout << "\n";
     }
-    add(head, array[0], 2);
-    for(int i = 1; i < a; i++) {
-      add(head, array[i], 0);
+    else if (strcmp(input, "File") == 0) {
+      cout << "Enter file name" << endl;
+      char fname[99];
+      cin.getline(fname, 99);
+      ifstream file;
+      file.open(fname);
+      char* output = new char[999];
+      int* array = new int[999];
+      int i = 0;
+      while (!file.eof()) {
+        file.get(output[i]);
+        i++;
+      }
+      char* split = strtok(output, " ");
+      int a = 0;
+      while (split!=NULL) {
+        array[a] = atoi(split);
+        split = strtok(NULL, " ");
+        ++a;
+      }
+      add(head, array[0], 2);
+      for (int i =0; i<a; i++) {
+        add(head, array[i], 0);
+      }
+      print(head, 0);
+      cout << "\n";
+    }
+    else if (strcmp(input, "Quit") == 0) {
+      quit = true;
     }
   }
-  else if (strcmp(input, "File") == 0) {
-    cout << "Enter file name" << endl;
-    char fname[99];
-    cin.getline(fname, 99);
-    ifstream file;
-    file.open(fname);
-    char* output = new char[999];
-    int* array = new int[999];
-    int i = 0;
-    while (!file.eof()) {
-      file.get(output[i]);
-      i++;
-    }
-    char* split = strtok(output, " ");
-    int a = 0;
-    while (split!=NULL) {
-      array[a] = atoi(split);
-      split = strtok(NULL, " ");
-      ++a;
-    }
-    add(head, array[0], 2);
-    for (int i =0; i<a; i++) {
-      add(head, array[i], 0);
-    }
+}
+
+void print(Node* head, int space) {
+  if (head == NULL) {
+    return;
   }
-  print(head);
+  space += 5;
+  if (head->getRight()!=NULL) {
+    print(head->getRight(), space);
+  }
+  cout << "\n";
+  for (int i = 5; i < space; i++) {
+    cout << " ";
+  }
+  cout << head->getValue() << endl;
+  if (head->getRight() != NULL) {
+    print(head->getLeft(), space);
+  }
 }
-
-void print(Node* head) {
-  cout << head->getRight()->getValue() << endl;
- 
-}
-
 void add(Node* parent, int value, int first) {
   int count = 0;
   if (first == 2) {
     parent -> setValue(value);
-  } 
+  }
   else {
     if (value >  parent->getValue()) {
       cout << "Going right" << endl;
