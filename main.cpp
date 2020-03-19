058f82203963e62324d0e06153c7fbabb46a0980
Ryan Thammakhoune <ryantmk518@gmail.com>
2020-03-19 04:48:57 +0100

    Finished search, working on remove

--- main.cpp
+++ main.cpp
@@ -3,17 +3,20 @@
 #include <cstring>
 #include <sstream>
 #include <fstream>
+#include "stdlib.h"
 
 using namespace std;
 
 void add(Node* parent, int value, int side);
 void print(Node* head, int space);
+void search(Node* head, int num);
+void remove(Node* head, int num);
 
 int main(){
   bool quit = false;
   Node* head = new Node(NULL);
   while (quit == false) {
-    cout << "Enter Console, File, or Quit" << endl;
+    cout << "Enter Console, File, Search, Remove, or Quit" << endl;
     char input[99];
     cin.getline(input, 99);
     if (strcmp(input, "Console") == 0) {
@@ -34,6 +37,7 @@ int main(){
         for(int i = 1; i < a; i++) {
           add(head, array[i], 0);
         }
+
       }
       else {
         for (int i =0; i<a; i++) {
@@ -70,9 +74,63 @@ int main(){
       print(head, 0);
       cout << "\n";
     }
+    else if (strcmp(input, "Search") == 0) {
+      char input[99];
+      cout << "Enter search integer" << endl;
+      cin.getline(input, 99);
+      int num = atoi(input);
+      search(head, num);
+    }
+    else if (strcmp(input, "Remove") == 0) {
+      char input[99];
+      cout << "Enter integer to remove" << endl;
+      cin.getline(input, 99);
+      cout << "\n" << endl;
+      int num = atoi(input);
+      remove(head, num);
+    }
     else if (strcmp(input, "Quit") == 0) {
       quit = true;
     }
+  }
+}
+
+void remove(Node* head, int num) {
+  if (head == NULL) {
+    return;
+  }
+  else if (head->getValue() < num) {
+    if (head->getRight()->getValue() = num && head->getRight()->getRight() != NULL && head->getRight()->getLeft() != NULL) {
+      twoChild(head, 0);
+    }
+    else if (head->getRight()->getValue() = num) {
+      if (head->getRight())
+    }
+    else {
+      remove(head->getRight(), num);
+    }
+  }
+  else if (head->getValue > num) {
+    remove (head->getLeft(), num);
+  }
+}
+
+void twoChild(Node* head, int direction) {
+  
+}
+
+void search(Node* head, int num) {
+  if (head == NULL) {
+    cout << "Not found" << endl;
+  }
+  else if (head->getValue() == num) {
+    cout << "Int is found" << "\n" << endl;
+  }
+  else if (head->getValue() < num) {
+    search(head->getRight(), num);
+  }
+  else if (head->getValue() > num) {
+    search(head->getLeft(), num);
   }
 }
 
@@ -92,15 +150,12 @@ void print(Node* head, int space){
 
 void add(Node* parent, int value, int first) {
   int count = 0;
-  if (first == 2) {
-    cout << "Set head" << endl;
+  if (first == 2) { //Set head
     parent -> setValue(value);
   }
   else {
-    if (value >  parent->getValue()) {
-      cout << "Going right" << endl;
+    if (value >  parent->getValue()) { //Going right
       if (parent->getRight() != NULL) {
-        cout << "Searching" << endl;
         add(parent -> getRight(), value, 0);
       }
       else {
@@ -110,13 +165,10 @@ void add(Node* parent, int value, int first) {
       }
     }
     else if (value < parent->getValue()) {
-      cout << "Going left" << endl;
-      if (parent -> getLeft() != NULL) {
-        cout << "Searching" << endl;
+      if (parent -> getLeft() != NULL) { // Going left
         add(parent->getLeft(), value, 0);
       }
-      else {
-        cout << "New node" << endl;
+      else { // New node
         Node* newNode = new Node(value);
         parent->setLeft(newNode);
       }
